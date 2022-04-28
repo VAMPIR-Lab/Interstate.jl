5c88be5a078658448f1b8461ade59932de13456d
erzz99 <erskinengugi99@gmail.com>
2022-04-28 11:28:21 -0500

    latest changes

--- src/localize.jl
+++ src/localize.jl
@@ -1,4 +1,40 @@
 
+function feasible_point(gps_position)
+    pointEstimate = gps_position
+    for i in gps_position
+    # generated rand +/-1 around gps_pos. Adjust rangeVal to adjust area covered
+        rangeVal = 2 #noise.std
+        pointEstimate[i] = rangeVal*rand() .+ (gps_position[i]-(rangeVal/2))
+     #make sure nothing is out of map bounds
+        if pointEstimate[i]>40
+            pointEstimate[i]=40
+        end
+    end
+
+    theta = (rand() * 2*pi) - pi
+    vel = rand() * 5
+
+    particle = [pointEstimate[1], pointEstimate[2], theta, vel]
+    return particle
+end
+
+
+
+function make_2d(points)
+    #3x2 matrix, each row stores points. Easier to access that point cloud
+    V = SVector{2, Float64}
+        #goes through every vector point
+    for i in points
+        #goes through the x and y value of every vector point
+        for j ∈ 1:2
+            V[i][j] = points[i][j]
+        end
+    end
+    V
+end
+
+
+
 function update_position!(p, deltat, u)
     new_particle = p
 
@@ -12,94 +48,111 @@ function update_position!(p, deltat, u)
     new_particle[3] += deltat * u[2]
     new_particle[4] += deltat * u[1]
 
+    return new_particle
 end 
 
 
 
-function localize(SENSE_LIDAR::Channel, SENSE_GPS::Channel, LOCALIZE::Channel, EGO_CMD::Channel, EMG::Channel, lidar, road, buildings)
-    meas = fetch(SENSE_LIDAR)
-    ego_meas = fetch(EGO_CMD)
-    gps = fetch(SENSE_GPS)
+function compute_range(pos, θ, buildings, max_beam_length; σ = 0.05)
+    c = cos(θ)
+    s = sin(θ)
+    α_min = max_beam_length
+    for seg ∈ buildings
 
-    # define time variable here and give it a default
-    initial_time = meas.time
-
-
-    while true
-        sleep(0)
-        @return_if_told(EMG)
-        
-        meas = @fetch_or_default(SENSE_LIDAR, meas)
-        ego_meas = @fetch_or_default(EGO_CMD, ego_meas)
-        gps = @fetch_or_default(SENSE_GPS, gps)
-        
-
-        #time since last iteration 
-        #need to capture the difference
-        #capture time here
-        deltat = rand()
-
-
-        # generate random particles within gps_position. 
-        # return for feasible point - [x,y] points for a possible article. 
-        # return for particles - [[x1,y1], [x2,y2], .... ]
-        particles = [feasible_point(gps.position) for _ in 1:100]
-
-        for particle in particles
-            update_position(particle, deltat, ego_meas);
-        end 
-      
-        #convert to 2D
-        dists = make_2d(meas.points)
-
-       
-        # origin::SVector{3, Float64}
-        d2_origin = [meas.orgin[1], meas.origin[2], 0, 0]
-
-
-        # speed is a delta 
-        # heading is a delta
-        new_position = update_position(d2_origin, deltat, ego_meas)
-        final_position = [new_position[1], new_position[2]]
-    
-
-        # return - 
-        # should we store the particles for the next iteration? 
-        # what do we do with this? 
-        update_particles!(particles, dists, deltat, buildings, ego_meas, lidar.max_beam_length)
-       
-        
-        # what do I do for the things I don't want to replace
-        # are we only replacing the new position? 
-        # is this speed and heading same as gps which means speed and heading was already exposed to us? 
-        # all these can be 0
-        ego_state = OracleMeas(final_position, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0)
-        
-
-        # capture system time
-        # time_elapsed = 
-        # subtract from time at the start and assign this to sth...
-        print(final_position) 
-        @replace(LOCALIZE, ego_state) 
-
-    end
-end
-
-
-function make_2d(points)
-#3x2 matrix, each row stores points. Easier to access that point cloud
-    V = SVector{2, Float64}
-    #goes through every vector point
-    for i in points
-        #goes through the x and y value of every vector point
-        for j ∈ 1:2
-            V[i][j] = points[i][j]
+        A = [c seg[2][1]-seg[1][1];
+             s seg[2][2]-seg[1][2]]
+        b = seg[2] - [pos[1],pos[2]]
+        α = A\b
+        if 0 ≤ α[2] ≤ 1 && α[1] ≥ 0 # beam intersects map segment
+            α_range = α[1] + randn()*σ
+            α_min = min(α_min, α_range)
         end
     end
-    V
+
+    r = rand()
+    if r < p_missed
+        α_min = α_max
+    elseif r < p_missed + p_scatter
+        α_min = α_max * rand()
+    end
+    (Point2(pos[1]+α_min*c, pos[2]+α_min*s), α_min)
 end
 
- 
+
+
+function sample_from(weights)
+    @assert sum(weights) ≈ 1.0 #make sure weights are normalized
+    @assert all(weights .≥ 0)
+
+    r = rand()
+    t = 0
+    ind = 0 
+    while t < r
+        ind += 1 
+        t += weights[ind]
+    end
+    ind
+end
+
+
+
+function expected_prob(α_meas, α_expec, σ, α_max)
+
+    if ((0 ≤ α_meas) && (α_meas ≤ α_max))
+        norm_constant = (0.5 * (1 +  erf((α_max - α_expec) / (σ * sqrt(2))))) - (0.5 * (1 +  erf((0 - α_expec) / (σ * sqrt(2)))))
+        return ((1 / (σ * sqrt(2 * π))) * exp(-0.5 * (((α_meas - α_expec)/σ)^2))) * (1/norm_constant)
+    else 
+        return 0
+    end
+end
+
+
+
+function scatter_prob(α_meas, α_max)
+    if ((0 ≤ α_meas) && (α_meas ≤ α_max))
+        return 1/α_max
+    else 
+        return 0.0
+    end
+end
+
+
+
+function missed_prob(α_meas, α_max)
+    if(α_meas == α_max)
+         return 1
+    else 
+        return 0
+    end
+end
+
+
+
+function measurement_prob(pos, 
+                          dists,
+                          buildings, 
+                          max_beam_length;
+                          σ=0.25,
+                          p_missed=0.001,
+                          p_scatter=0.001,
+                          sub_sample_factor::Int=1)
+    Nb = length(dists)
+    p = 1.0
+    p_expected = 1.0 - p_missed - p_scatter
+    for n ∈ 1:sub_sample_factor:Nb
+        θ = 2*π*n/Nb
+        _, expected_dist = compute_range(pos, θ, buildings, max_beam_length; σ=0.0, p_missed=0.0, p_scatter=0.0)
+        measured_dist = dists[n]
+        pₙ = p_expected * expected_prob(measured_dist, expected_dist, σ, max_beam_length) + 
+             p_missed * missed_prob(measured_dist, max_beam_length) +
+             p_scatter * scatter_prob(measured_dist, max_beam_length)
+        p *= pₙ
+    end
+    p  
+end
+
+
+
 function update_particles!(particles, dists, delta, buildings, max_beam_length; σ_move=0.3, ssf = 10, p_birth=0.1)
     candidate_particles = []
     candidate_weights = []
@@ -134,117 +187,80 @@ end
 
 
 
-function feasible_point(gps_position)
-    pointEstimate = gps_position
-    for i in gps_position
-    # generated rand +/-1 around gps_pos. Adjust rangeVal to adjust area covered
-        rangeVal = 2 #noise.std
-        pointEstimate[i] = rangeVal*rand() .+ (gps_position[i]-(rangeVal/2))
-     #make sure nothing is out of map bounds
-        if pointEstimate[i]>40
-            pointEstimate[i]=40
-        end
-    end
+function localize(SENSE_LIDAR::Channel, SENSE_GPS::Channel, LOCALIZE::Channel, EGO_CMD::Channel, EMG::Channel, lidar, road, buildings)
+    meas = fetch(SENSE_LIDAR)
+    ego_meas = fetch(EGO_CMD)
+    gps = fetch(SENSE_GPS)
 
-    theta = (rand() * 2*pi) - pi
-    vel = rand() * 5
+    # define time variable here and give it a default
+    #initial_time = meas.time
 
-    particle = [pointEstimate[1], pointEstimate[2], theta, vel]
-    return particle
-end
+    Δ=0.001
+    t0 = time_ns()
+    deltat = 0.0
 
+    while true
+        sleep(0)
+        @return_if_told(EMG)
+        
+        meas = @fetch_or_default(SENSE_LIDAR, meas)
+        ego_meas = @fetch_or_default(EGO_CMD, ego_meas)
+        gps = @fetch_or_default(SENSE_GPS, gps)
+        
 
+        #time since last iteration 
+        #need to capture the difference
+        #capture time here
+        #deltat = rand()
 
-function compute_range(pos, θ, buildings, max_beam_length; σ = 0.05)
-    c = cos(θ)
-    s = sin(θ)
-    α_min = max_beam_length
-    for seg ∈ buildings
+        deltat += Δ
 
-        A = [c seg[2][1]-seg[1][1];
-             s seg[2][2]-seg[1][2]]
-        b = seg[2] - [pos[1],pos[2]]
-        α = A\b
-        if 0 ≤ α[2] ≤ 1 && α[1] ≥ 0 # beam intersects map segment
-            α_range = α[1] + randn()*σ
-            α_min = min(α_min, α_range)
-        end
-    end
+        # generate random particles within gps_position. 
+        # return for feasible point - [x,y] points for a possible article. 
+        # return for particles - [[x1,y1], [x2,y2], .... ]
+        particles = [feasible_point(gps.position) for _ in 1:100]
 
-    r = rand()
-    if r < p_missed
-        α_min = α_max
-    elseif r < p_missed + p_scatter
-        α_min = α_max * rand()
-    end
-    (Point2(pos[1]+α_min*c, pos[2]+α_min*s), α_min)
-end
+        for particle in particles
+            update_position(particle, deltat, ego_meas);
+        end 
+      
+        #convert to 2D
+        dists = make_2d(meas.points)
 
+       
+        # origin::SVector{3, Float64}
+        d2_origin = [meas.orgin[1], meas.origin[2], 0, 0]
 
-function expected_prob(α_meas, α_expec, σ, α_max)
+        new_position = update_position(d2_origin, deltat, ego_meas)
+        final_position = [new_position[1], new_position[2]]
+    
+        update_particles!(particles, dists, deltat, buildings, ego_meas, lidar.max_beam_length)
+       
+        ego_state = OracleMeas(final_position, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0)
+        print(final_position) 
+        @replace(LOCALIZE, ego_state) 
 
-    if ((0 ≤ α_meas) && (α_meas ≤ α_max))
-        norm_constant = (0.5 * (1 +  erf((α_max - α_expec) / (σ * sqrt(2))))) - (0.5 * (1 +  erf((0 - α_expec) / (σ * sqrt(2)))))
-        return ((1 / (σ * sqrt(2 * π))) * exp(-0.5 * (((α_meas - α_expec)/σ)^2))) * (1/norm_constant)
-    else 
-        return 0
-    end
-end
+        err = (time_ns()-t0)/1e9 - deltat
+        Δ = max(0.0, min(5e-1, err))
 
-
-function missed_prob(α_meas, α_max)
-    if(α_meas == α_max)
-         return 1
-    else 
-        return 0
     end
 end
 
 
-function scatter_prob(α_meas, α_max)
-    if ((0 ≤ α_meas) && (α_meas ≤ α_max))
-        return 1/α_max
-    else 
-        return 0.0
-    end
-end
 
 
  
-function measurement_prob(pos, 
-                          dists,
-                          buildings, 
-                          max_beam_length;
-                          σ=0.25,
-                          p_missed=0.001,
-                          p_scatter=0.001,
-                          sub_sample_factor::Int=1)
-    Nb = length(dists)
-    p = 1.0
-    p_expected = 1.0 - p_missed - p_scatter
-    for n ∈ 1:sub_sample_factor:Nb
-        θ = 2*π*n/Nb
-        _, expected_dist = compute_range(pos, θ, buildings, max_beam_length; σ=0.0, p_missed=0.0, p_scatter=0.0)
-        measured_dist = dists[n]
-        pₙ = p_expected * expected_prob(measured_dist, expected_dist, σ, max_beam_length) + 
-             p_missed * missed_prob(measured_dist, max_beam_length) +
-             p_scatter * scatter_prob(measured_dist, max_beam_length)
-        p *= pₙ
-    end
-    p  
-end
 
 
-function sample_from(weights)
-    @assert sum(weights) ≈ 1.0 #make sure weights are normalized
-    @assert all(weights .≥ 0)
 
-    r = rand()
-    t = 0
-    ind = 0 
-    while t < r
-        ind += 1 
-        t += weights[ind]
-    end
-    ind
-end
+
+
+
+
+
+
+
+
+
+
+
